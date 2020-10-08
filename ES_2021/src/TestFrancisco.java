0beeab817adfef8a4883554ad0e821f641242c68
fafaa-iscte <fafaa@iscte-iul.pt>
2020-10-08 17:30:02 +0100

    mandei push

--- ES_2021/src/TestFrancisco.java
+++ ES_2021/src/TestFrancisco.java
@@ -4,6 +4,7 @@ public class TestFrancisco {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Francisco: Fiz qualquer coisa.");
+		System.out.println("Francisco: Fiz qualquer coisa.");		
 	}
 
 }
